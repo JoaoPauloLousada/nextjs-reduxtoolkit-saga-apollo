e1ed8f8310ec07b6dd7a2d7f3e15e950bec9434e
Joao Paulo Lousada <joao@imaginationmedia.com>
2020-09-10 22:32:04 -0300

    add apollo

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import { Provider } from "react-redux";
+import { ApolloProvider } from "@apollo/client";
+import { useApollo } from "../lib/apollo";
 
 import store from "../lib/redux/store";
 
 const MyApp = ({ Component, pageProps }) => {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </Provider>
   );
 };
